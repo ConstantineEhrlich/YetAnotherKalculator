b8cd46756cb285408231c0157c6aeb58de0fe2e5
Ehrlich Constantine <constantine.ehrlich@elbitsystems.com>
2023-10-20 23:17:17 +0300

    Calculator backend logic

---
+++ CalculatorModel/Calculator.cs
@@ -0,0 +1,49 @@
+﻿namespace CalculatorModel
+{
+    /// <inheritdoc/>
+    public class Calculator : ICalculator
+    {
+        // Stores the operation. Default operation => return display value.
+        private Func<double, double, double> _operation = _defaultOperation;
+        private static double _defaultOperation(double display, double memory) => display;
+
+        // Initialize both registers with zero
+        private double _memory = 0;
+        private double _display = 0;
+        public double MemoryRegister => _memory;
+        public double DisplayRegister => _display;
+
+
+        public double? Evaluate()
+        {
+            double result;
+            try
+            {
+                result = _operation(_display, _memory);
+                // If the result is NaN, return null, else return the result
+                return double.IsNaN(result) ? null : result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public void SetOperation(Func<double, double, double> operation)
+        {
+            _operation = operation;
+        }
+
+
+        public void SetOperation(Func<double, double> operation)
+        {
+            _operation = (display, memory) => operation(display);
+        }
+
+        public void SetDisplayValue(double value)
+        {
+            _memory = _display;
+            _display = value;
+        }
+    }
+}
---
+++ UnitTests/CalculatorModelTests.cs
@@ -0,0 +1,39 @@
+using CalculatorModel;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CalculatorModelTests
+    {
+        [TestMethod]
+        public void EvaluateBinaryOperation()
+        {
+            Calculator calc = new();
+            calc.SetDisplayValue(1.5);
+            calc.SetOperation((display, memory) => display + memory);
+            calc.SetDisplayValue(2.5);
+            Assert.AreEqual(4.0, calc.Evaluate());
+        }
+
+        [TestMethod]
+        public void EvaluateUnaryOperation()
+        {
+            Calculator calc = new();
+            calc.SetDisplayValue(4);
+            calc.SetOperation(display => Math.Sqrt(display));
+            Assert.AreEqual(2, calc.Evaluate());
+        }
+
+
+        [TestMethod]
+        public void ReturnNullForIllegalOp()
+        {
+            Calculator calc = new();
+            calc.SetDisplayValue(-5);
+            calc.SetOperation(val => Math.Sqrt(val));
+            Assert.IsNull(calc.Evaluate());
+        }
+    }
+}
